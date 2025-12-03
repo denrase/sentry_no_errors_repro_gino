4b52135db7de8cdd1212bf294a54a6c5ccbcbe72
Denis Andrasec <denrase@gmail.com>
2025-12-03 14:01:39 +0100

    gino repro

---
+++ android/app/src/main/kotlin/com/example/test_text_field/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_text_field
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+Future<void> main() async {
+  await SentryFlutter.init((options) {
+    options.dsn =
+        'https://f053b4bba1216dbc7cca84f18d246b1c@o376011.ingest.us.sentry.io/4508958566055936';
+  }, appRunner: () => runApp(const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Path Provider Crash Repro')),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(),
+                const Center(child: CircularProgressIndicator()),
+                FilledButton(
+                  onPressed: () {
+                    try {
+                      throw Exception('Test exception');
+                    } catch (e, s) {
+                      Sentry.captureException(e, stackTrace: s);
+                    }
+                  },
+                  child: Text('click me'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
